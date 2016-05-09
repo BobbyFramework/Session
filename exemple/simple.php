907ebf97b075bcb38ebb2383f5e03d85b53cb485
nicolas <n.wallerand@gmail.com>
2016-05-09 14:08:23 +0200

    add exemple simple

--- exemple/simple.php
+++ exemple/simple.php
@@ -3,3 +3,9 @@
 define('APP_PATH',realpath('..'));
 
 require APP_PATH . '/vendor/autoload.php';
+
+$session = new \BobbyFramework\Core\Components\Session\Session();
+
+$session->setAttribute('test',"jksQKJSKqlslqs");
+
+echo $session->getAttribute('test');
